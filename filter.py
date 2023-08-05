d913a15b5addc955fe8a51817c4a74d7c3500b67
Maique <janofstargard@gmail.com>
2023-08-05 02:59:05 -0700

    SYN flood filter, implement SYN logic

--- filter.py
+++ filter.py
@@ -71,7 +71,83 @@ def main():
                 print("no")
 
     elif option == "-k":
-        print("k")
+        # If single outside IP attempts to half-open more than 10 connections with a host in the subnet,
+        # filter out all further SYN packets until the total number of half-open connections is reduced below 10
+
+        # ASSUMPTIONS
+
+        # Assume that a host on our subnet is a (destination_address, destination port)
+
+        # If a SINGLE IP attempts to half-open more than 10 connections with a host, all further SYN packets are dropped
+        # until the number of half-open connections to that host is reduced below 10
+
+        # This means that the only way for a specific counter on a specific port in our subnet to reach 10 half-connections
+        # is if a single outside IP address uses 10 different ports to open half connections with the our single subnet port
+
+        # we can keep track of each counter in a dictionary. Each counter has a key (destination_address, destination_port, source_address)
+        # so connections = {(destination_address, destination_port, source_address):[source_port]}
+        # the values attached to each key is a list of ports that are connected (or half-connected) on the source_address
+        half_open_connections = {}
+        open_connections = []
+        flooded = False
+
+        i = 1
+        for packet in packets:
+            if int(packet[9]) == 6:
+                source_address = ''.join(f'{byte:08b}' for byte in packet[12:16])
+                destination_address = ''.join(f'{byte:08b}' for byte in packet[16:20])
+                source_port = ''.join(f'{byte:08b}' for byte in packet[20:22])
+                destination_port = ''.join(f'{byte:08b}' for byte in packet[22:24])
+                # sequence number packet[24:28]
+                # ack number packet[28:32]
+                flag_bits = ''.join(f'{byte:08b}' for byte in packet[33])
+                SYN = int(flag_bits[6])
+                ACK = int(flag_bits[3])
+                RST = int(flag_bits[5])
+                FIN = int(flag_bits[7])
+                # SYN msg
+                if SYN == 1 and ACK == 0:
+                    # add half-open connection
+                    # block if flooded
+                    half_connection_key = (destination_address, destination_port, source_address)
+                    full_connection = (destination_address, destination_port, source_address, source_port)
+                    if flooded:
+                        print("yes")
+                    elif full_connection in open_connections:
+                        print("no")
+                    elif half_connection_key in half_open_connections:
+                        source_ports = half_open_connections[half_connection_key]
+                        if source_port in source_ports:
+                            print("no")
+                        elif len(source_ports) == 10:
+                            flooding = True
+                            print("yes")
+                        else:
+                            source_ports.append(source_port)
+                            half_open_connections[half_connection_key] = source_ports
+                            print("no")
+                    else:
+                        #create half connection
+                        half_open_connections[half_connection_key] = [source_port]
+                        print("no")
+
+                # SYN-ACK
+                elif SYN == 1 and ACK == 1:
+                    print("no")
+                # ACK
+                elif SYN == 0 and ACK == 1:
+                    # ACK msg, make full from half-connection
+                    # check flooding
+                # RST/FIN
+                elif RST == 1 or FIN == 1:
+                    # cut the connection half or full
+                    # check flooding
+                else:
+                    print("no")
+            else:
+                print("no")
+            i += 1
+
     else:
         print("Invalid option")
 
